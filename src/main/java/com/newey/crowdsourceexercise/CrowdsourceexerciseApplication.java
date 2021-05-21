12ac46aebe66774640215b5a91e49a0a4c46582b
Cory <cory.newey@zpaper.com>
2021-05-21 16:44:48 -0600

    Adding preliminary Spring code.

---
+++ src/main/java/com/newey/crowdsourceexercise/CrowdsourceexerciseApplication.java
@@ -0,0 +1,13 @@
+package com.newey.crowdsourceexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowdsourceexerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrowdsourceexerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/newey/crowdsourceexercise/CrowdsourceexerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newey.crowdsourceexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrowdsourceexerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
